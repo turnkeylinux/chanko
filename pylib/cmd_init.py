a0083c77bb81293577c61653e26aea747265464f
Alon Swartz <alon@turnkeylinux.org>
2011-03-09 13:42:01 +0200

    bugfix: dummy sourceslist should be found according to 'INSTALL_PATH'

--- pylib/cmd_init.py
+++ pylib/cmd_init.py
@@ -10,19 +10,36 @@ If --dummy is specified, an exemplary sources.list will be created.
 
 """
 import sys
+from os.path import *
+
 import container
 import help
+
+class Error(Exception):
+    pass
 
 @help.usage(__doc__)
 def usage():
     print >> sys.stderr, "Syntax: %s /path/to/sources.list | --dummy" % sys.argv[0]
+
+def get_dummy_sourceslist():
+    INSTALL_PATH = dirname(dirname(__file__))
     
+    paths = (join(INSTALL_PATH, 'contrib/sources.list'),
+             join(dirname(INSTALL_PATH), 'share/chanko/contrib/sources.list'))
+
+    for path in paths:
+        if exists(path):
+            return path
+
+    raise Error('dummy sources.list file not found', paths)
+
 def main():
     if len(sys.argv) != 2:
         usage()
 
     if sys.argv[1] == "--dummy":
-        sourceslist = "/usr/share/chanko/sources.list"
+        sourceslist = get_dummy_sourceslist()
         refresh = False
     else:
         sourceslist = sys.argv[1]
