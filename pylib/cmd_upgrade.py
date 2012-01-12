b8bc76f22fc758980f503c73cda39f3725e389bb
Alon Swartz <alon@turnkeylinux.org>
2012-01-12 17:29:17 +0200

    added chanko-upgrade --nodeps option

--- pylib/cmd_upgrade.py
+++ pylib/cmd_upgrade.py
@@ -5,6 +5,8 @@ Upgrade chanko archives according to log
 Options:
   -p --purge     Purge superceded archives
   -f --force     Dont ask for confirmation before downloading and purging
+  -n --nodeps    Do not get package dependencies
+
 """
 
 import sys
@@ -22,17 +24,21 @@ def usage():
     
 def main():
     try:
-        opts, args = getopt.gnu_getopt(sys.argv[1:], ":f", ['force'])
+        opts, args = getopt.gnu_getopt(sys.argv[1:], ":fpn", 
+                                       ['force', 'purge', 'nodeps'])
     except getopt.GetoptError, e:
         usage(e)
 
     opt_force = False
     opt_purge = False
+    opt_nodeps = False
     for opt, val in opts:
         if opt in ('-f', '--force'):
             opt_force = True
         elif opt in ('-p', '--purge'):
             opt_purge = True
+        elif opt in ('-n', '--nodeps'):
+            opt_nodeps = True
 
     chanko = Chanko()
 
@@ -40,7 +46,7 @@ def main():
     chanko.local_cache.refresh()
 
     toget = promote_depends(chanko.remote_cache, chanko.log.list())
-    if chanko.remote_cache.get(toget, opt_force):
+    if chanko.remote_cache.get(toget, opt_force, nodeps=opt_nodeps):
         if opt_purge:
             purge(chanko.paths.archives, opt_force)
 
